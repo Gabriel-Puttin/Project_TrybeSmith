3613f6739d9c06c6d006da9a7fc6c865ce9011e2
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-12-20 16:43:39 -0300

    Finished requirement 06

--- src/services/validations/productsValidation.ts
+++ src/services/validations/productsValidation.ts
@@ -2,13 +2,14 @@ import { Products } from '../../interfaces/products.interface';
 import { productsSchema } from './schemas';
 
 const HTTP_BAD_REQUEST_STATUS = 400;
+const HTTP_UNPROCESSABLE_STATUS = 422;
 
 const validateProduct = (product: Products) => {
   const { name, amount } = product;
   const { error } = productsSchema.validate(product);
   if (!name) return { type: HTTP_BAD_REQUEST_STATUS, message: '"name" is required' };
   if (!amount) return { type: HTTP_BAD_REQUEST_STATUS, message: '"amount" is required' };
-  if (error) return { type: HTTP_BAD_REQUEST_STATUS, message: error.message };
+  if (error) return { type: HTTP_UNPROCESSABLE_STATUS, message: error.message };
 };
 
 export default validateProduct;
